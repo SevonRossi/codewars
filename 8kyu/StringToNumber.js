254fd50bd66d82d91eac46bea461d2dba10317d7
Srossi28 <105078485+Srossi27@users.noreply.github.com>
2022-12-27 12:00:02 -0600

    adjusting files

--- 8kyu/StringToNumber.js
+++
@@ -1,5 +0,0 @@
-// We need a function that can transform a string into a number. What ways of achieving this do you know?
-
-const stringToNumber = function(str){
- return parseInt(str);
-}
--- 8kyu/repeat.js
+++
@@ -1,7 +0,0 @@
-// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
-
-function repeatStr(n, s) {
-  let word = s;
-  return word.repeat(n);
-}
-  
