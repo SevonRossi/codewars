e45dad4bcc17c0250f66eb35fc6e51cc6d7ee30f
Srossi28 <105078485+Srossi27@users.noreply.github.com>
2022-12-28 11:27:43 -0600

    cs codewar

---
+++ 8kyu-C#/hydrated.cs
@@ -0,0 +1,18 @@
+// Nathan loves cycling.
+
+// Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+// For example:
+
+using System;
+
+public class Kata
+{
+  public static int Litres(double time)
+  {
+    //The fun begins here.
+    return (int) (time/2);
+  }
+}
